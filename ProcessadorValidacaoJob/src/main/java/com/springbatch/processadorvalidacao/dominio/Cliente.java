0dce0a6feccc03d7d331a59e1d5d3854cde8d7b0
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-10 09:41:00 -0300

    aula 45 processadores de validação

--- ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/dominio/Cliente.java
+++ ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/dominio/Cliente.java
@@ -1,9 +1,26 @@
 package com.springbatch.processadorvalidacao.dominio;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Cliente {
 
+    @NotBlank
+    @Size(min = 1, max = 100)
+    @Pattern(regexp = "[a-zA-Z\\s]+", message = "Nome deve ser alfabético")
     private String nome;
+
+    @NotNull
+    @Range(min = 18, max = 200)
     private Integer idade;
+
+    @NotBlank
+    @Email(message = "Email inválido")
     private String email;
 
     public String getNome() {
--- ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
+++ ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -2,16 +2,40 @@ package com.springbatch.processadorvalidacao.processor;
 
 import com.springbatch.processadorvalidacao.dominio.Cliente;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.String.format;
 
 @Configuration
 public class ProcessadorValidacaoProcessorConfig {
 
+    private final Set<String> emails = new HashSet<>();
+
     @Bean
     public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
-        // TODO: Implementar a lógica aqui...
-        return cliente -> cliente;
+//        BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+//        processor.setFilter(true); // filtra os inválidos
+        ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>(); // validacao customizada
+        processor.setValidator(validator());
+        processor.setFilter(true); // filtra os inválidos
+        return processor;
+    }
+
+    private Validator<Cliente> validator() {
+        return cliente -> {
+            String clienteEmail = cliente.getEmail();
+            if (emails.contains(clienteEmail)) {
+                throw new ValidationException(format("O cliente %s já foi processado", clienteEmail));
+            }
+            emails.add(clienteEmail);
+        };
     }
 
 }
